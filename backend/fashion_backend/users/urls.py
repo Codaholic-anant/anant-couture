bb579f7450fda4baf5c966a0e0e20dc4070089c2
Anant Deep <ind.anantdeep@gmail.com>
2026-03-16 07:17:09 +0530

    Fix users urls

--- backend/fashion_backend/users/urls.py
+++ backend/fashion_backend/users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, create_superuser
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    # path('create-superuser/', create_superuser, name='create_superuser'),
 ]
